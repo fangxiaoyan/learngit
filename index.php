60321808bd7b2922d2541a9e820cfabb1048923d
fxy <fangxiaoyan@kongzhong.com>
2017-03-23 11:48:19 +0800

    乱写

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?
+git config user.name = "";
+git config user.email ="";
+
 1 创建文件夹  mkdir （winows非中文）
 2 创建仓库  git init
 3 添加文件  git add <file>
@@ -6,5 +9,8 @@
 5 查看不同  git diff
 6 提交修改和提交新文件是一样的 git add  <file>
 7 查看提交的版本 git log
+8 版本列表 git log --pretty=oneline
 
+单词 ：branch 分支  stage 上演显示 tracked 追踪  pretty完美 directory仓库 
+单词啊 单词
 ?>
